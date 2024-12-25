3947612af4e8d89ba2878f8b202b597be17dab31
krygz <ph4290245@gmail.com>
2024-12-25 10:45:58 -0300

    set hash

--- block/block.go
+++ block/block.go
@@ -1,8 +1,35 @@
 package block
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// Block keeps block headers
 type Block struct {
 	TimeStamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+}
+
+// SetHash calculates and sets block hash
+func (b *Block) setHash() {
+	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func newBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		time.Now().Unix(),
+		[]byte(data),
+		prevBlockHash,
+		[]byte{}}
+	block.setHash()
+	return block
 }
